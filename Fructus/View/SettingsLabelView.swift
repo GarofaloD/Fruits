c9d13c82a8cdaf1505f5921649a25ff374d95341
Daniel  Garofalo <danielgarofalo@daniels-mbp.home>
2020-09-26 07:57:33 -0500

    SettingsLabelView created and imported into Settings view

---
+++ Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by Daniel  Garofalo on 9/26/20.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView()
+    }
+}
